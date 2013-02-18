f02db60dc64845b20b5aec0520f4caadba7f316e
Douglas Gardner <douglas@chippy.ch>
2013-02-18 00:23:43 +0000

    add extensions

--- extensions.php
+++ extensions.php
@@ -1,2 +1,12 @@
 <?php
 require_once( "$IP/extensions/ParserFunctions/ParserFunctions.php" );
+
+require("$IP/extensions/ConfirmEdit/ConfirmEdit.php");
+require_once("$IP/extensions/ConfirmEdit/Asirra.php");
+$wgCaptchaClass = 'Asirra';
+require("$IP/extensions/WikiEditor/WikiEditor.php");
+
+# SyntaxHighlight_GeSHi
+# https://www.mediawiki.org/wiki/Extension:SyntaxHighlight_GeSHi
+
+require_once("$IP/extensions/SyntaxHighlight_GeSHi/SyntaxHighlight_GeSHi.php");
---
+++ installext.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "Welcome to the extension installer.";
+echo "Please enter the name of the extension you wish to install:";
+read name
+git submodule add https://gerrit.wikimedia.org/r/p/mediawiki/extensions/$name.git
+git submodule update
+echo "\n# $name\n# https://www.mediawiki.org/wiki/Extension:$name\n\nrequire_once(\"\$IP/extensions/$name/$name.php\");" >> extensions.php
