109c12a95745298b6d1683a0479b032f33dc8517
Douglas Gardner <douglas@chippy.ch>
2013-02-18 16:21:59 +0000

    update installation script

--- installext.sh
+++ installext.sh
@@ -5,3 +5,6 @@ read name
 git submodule add https://gerrit.wikimedia.org/r/p/mediawiki/extensions/$name.git
 git submodule update
 echo "\n# $name\n# https://www.mediawiki.org/wiki/Extension:$name\nrequire_once(\"\$IP/extensions/$name/$name.php\");" >> extensions.php
+git commit -m "Automatically install $name extension"
+git push
+
