7963e3c5ada9852e94bb5d73bbef4cdc84ad5942
Douglas Gardner <douglas@chippy.ch>
2014-06-20 17:53:41 +0000

    Automatically install Renameuser extension

--- extensions.php
+++ extensions.php
@@ -141,10 +141,10 @@ if ( !isset( $zuLocal ) ) {
 # https://www.mediawiki.org/wiki/Extension:intersection
 require_once("$IP/extensions/Intersection/DynamicPageList.php");
 
-# CLDR
-# https://www.mediawiki.org/wiki/Extension:CLDR
-require_once("$IP/extensions/CLDR/CLDR.php");
-
 # cldr
 # https://www.mediawiki.org/wiki/Extension:cldr
 require_once("$IP/extensions/cldr/cldr.php");
+
+# Renameuser
+# https://www.mediawiki.org/wiki/Extension:Renameuser
+require_once("$IP/extensions/Renameuser/Renameuser.php");
